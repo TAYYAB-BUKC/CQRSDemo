3d6d0a055200b86996b1eb1e84e689add8f203d7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-15 09:48:05 +0500

    New methods implemented for DataAccess.

--- CQRSDemo.Helper/DataAccess/DemoDataAccess.cs
+++ CQRSDemo.Helper/DataAccess/DemoDataAccess.cs
@@ -12,5 +12,17 @@ namespace CQRSDemo.Helper.DataAccess
             People.Add(new Person { Id = 2, FirstName = "Noman", LastName = "Khalid" });
             People.Add(new Person { Id = 3, FirstName = "Tahir", LastName = "Jaffar" });
         }
+
+        public List<Person> GetPeople()
+        {
+            return People;
+        }
+
+        public Person InsertPerson(string firstName, string lastName)
+        {
+            Person newPerson = new() { Id = People.Count == 0 ? 1 : People.Max(p => p.Id) + 1, FirstName = firstName, LastName = lastName };
+            People.Add(newPerson);
+            return newPerson;
+        }
     }
 }
