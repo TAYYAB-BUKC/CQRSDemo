9b886e543e3d2f43b0f492e796b3d139852bfa56
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-15 09:31:58 +0500

    DataAccess added.

---
+++ CQRSDemo.Helper/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using CQRSDemo.Helper.Models;
+
+namespace CQRSDemo.Helper.DataAccess
+{
+    public class DemoDataAccess
+    {
+        public List<Person> People = new();
+
+        public DemoDataAccess()
+        {
+            People.Add(new Person { Id = 1, FirstName = "Tayyab", LastName = "Arsalan" });
+            People.Add(new Person { Id = 2, FirstName = "Noman", LastName = "Khalid" });
+            People.Add(new Person { Id = 3, FirstName = "Tahir", LastName = "Jaffar" });
+        }
+    }
+}
