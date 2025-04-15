b5a4c4b238e5ed539086f7534a0acc9d35a07f99
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-15 10:20:13 +0500

    GetPersonList query and handler added.

---
+++ CQRSDemo.Helper/Handlers/GetPersonListHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRSDemo.Helper.DataAccess;
+using CQRSDemo.Helper.Models;
+using CQRSDemo.Helper.Queries;
+using MediatR;
+
+namespace CQRSDemo.Helper.Handlers
+{
+    public class GetPersonListHandler : IRequestHandler<GetPersonListQuery, List<Person>>
+    {
+        private readonly IDemoDataAccess _demoDataAccess;
+        public GetPersonListHandler(IDemoDataAccess demoDataAccess)
+        {
+            _demoDataAccess = demoDataAccess;
+        }
+
+        public Task<List<Person>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_demoDataAccess.GetPeople());
+        }
+    }
+}
---
+++ CQRSDemo.Helper/Queries/GetPersonListQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRSDemo.Helper.Models;
+using MediatR;
+
+namespace CQRSDemo.Helper.Queries
+{
+    public record GetPersonListQuery : IRequest<List<Person>>
+    {
+    }
+}
