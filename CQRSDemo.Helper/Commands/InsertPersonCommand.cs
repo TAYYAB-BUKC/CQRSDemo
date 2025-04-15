53f3b89d8ecea5a0907a4df0e7373c831504b309
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-15 12:40:06 +0500

    InsertPerson command and handler implemented.

---
+++ CQRSDemo.Helper/Commands/InsertPersonCommand.cs
@@ -0,0 +1,17 @@
+﻿using CQRSDemo.Helper.Models;
+using MediatR;
+
+namespace CQRSDemo.Helper.Commands
+{
+    public class InsertPersonCommand : IRequest<Person>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public InsertPersonCommand(string? firstName, string? lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ CQRSDemo.Helper/Handlers/InsertPersonHandler.cs
@@ -0,0 +1,21 @@
+﻿using CQRSDemo.Helper.Commands;
+using CQRSDemo.Helper.DataAccess;
+using CQRSDemo.Helper.Models;
+using MediatR;
+
+namespace CQRSDemo.Helper.Handlers
+{
+    public class InsertPersonHandler : IRequestHandler<InsertPersonCommand, Person>
+    {
+        private readonly IDemoDataAccess _demoDataAccess;
+        public InsertPersonHandler(IDemoDataAccess demoDataAccess)
+        {
+            _demoDataAccess = demoDataAccess;
+        }
+
+        public Task<Person> Handle(InsertPersonCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_demoDataAccess.InsertPerson(request.FirstName, request.LastName));
+        }
+    }
+}
