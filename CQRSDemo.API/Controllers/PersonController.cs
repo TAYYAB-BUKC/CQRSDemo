146693229f3bf75618e2520d199e31fecf92e788
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-04-15 12:21:40 +0500

    Get endpoint implemented.

---
+++ CQRSDemo.API/Controllers/PersonController.cs
@@ -0,0 +1,42 @@
+﻿using CQRSDemo.Helper.Models;
+using CQRSDemo.Helper.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CQRSDemo.API.Controllers
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PersonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<PersonController>
+        [HttpGet]
+        public async Task<IEnumerable<Person>> Get()
+        {
+            return await _mediator.Send(new GetPersonListQuery());
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
